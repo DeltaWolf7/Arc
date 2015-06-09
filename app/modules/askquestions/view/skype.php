dd54090a2f4ee745b5fb1820f660528daa351ead
Craig <Craig@X51>
2015-06-09 18:52:21 +0100

    Updates

--- app/modules/askquestions/view/skype.php
+++ app/modules/askquestions/view/skype.php
@@ -5,34 +5,36 @@
 
 <form>
     <div class="form-group">
-        <label for="day">When (within 10 days)</label>
-        <select type="text" class="form-control" id="day">
+        <label for="day">When (within 10 days, excluding Monday and Sunday)</label>
+        <select type="text" class="form-control" id="day" onchange="dateChange();">
             <?php
             $date = date("d-m-Y");
             $count = 1;
-            while ($count < 10) {
+            while ($count <= 10) {
                 $date = strtotime(date("d-m-Y", strtotime($date)) . " +1 day");
-                $date = date("d-m-Y",$date);
-                echo "<option>{$date}</option>";
-                $count++;
+                $date = date("d-m-Y", $date);
+                // dont allow mondays/sundays
+                if ((date("w", strtotime($date)) != 1) && (date("w", strtotime($date)) != 0)) {
+                    echo "<option>{$date}</option>";
+                    $count++;
+                }
             }
             ?>
         </select>
     </div>
     <div class="form-group">
         <label for="time">Time</label>
-        <select type="text" class="form-control" id="time">
-            <option>10:00</option>
-            <option>10:30</option>
-            <option>11:00</option>
-            <option>11:30</option>
-            <option>12:00</option>
-            <option>12:30</option>     
-            <option>13:00</option>
-            <option>13:30</option>
-            <option>14:00</option>
-            <option>14:30</option>
+        <select type="text" class="form-control" id="time" onchange="timeChange()">
+            <option>16:00</option>
+            <option>16:30</option>
+            <option>17:00</option>
+            <option>17:30</option>
+            <option>18:00</option>
         </select>
+    </div>
+    <div class="form-group">
+        <label for="ds">Booking Information</label>
+        <div id="ds" class="form-control">Not selected</div>
     </div>
     <button type="button" class="btn btn-default" onclick="send()">Submit</button>
 </form>
@@ -52,4 +54,37 @@
             }
         });
     }
+
+    function dateChange() {
+        var data = $("#day").val().split("-");
+        var time = $("#time").val().split(":");
+        // small hack to correct month being out by 1.
+        var today = new Date(data[2], data[1] - 1, data[0], time[0], time[1], 00);
+        if (today.getDay() == 6) {
+            $("#time")
+                    .empty()
+                    .append('<option selected="selected">10:00</option><option>10:30</option><option>11:00</option><option>11:30</option><option>12:00</option><option>12:30</option><option>13:00</option><option>13:30</option><option>14:00</option><option>14:30</option><option>15:00</option>');
+        } else {
+            $("#time")
+                    .empty()
+                    .append('<option selected="selected">16:00</option><option>16:30</option><option>17:00</option><option>17:30</option><option>18:00</option>');
+        }
+        $("#ds")
+                .empty()
+                .append('Date selected: ' + today);
+    }
+
+    function timeChange() {
+        var data = $("#day").val().split("-");
+        var time = $("#time").val().split(":");
+        // small hack to correct month being out by 1.
+        var today = new Date(data[2], data[1] - 1, data[0], time[0], time[1], 00);
+        $("#ds")
+                .empty()
+                .append('Date selected: ' + today);
+    }
+    
+    $(document).ready(function () {
+        timeChange();
+    });
 </script>
---
+++ app/templates/delta/view/footer.php
@@ -0,0 +1,9 @@
+</div>
+<div class="footer">
+    <div class="container">
+        <p class="text-muted">Arc Project Version <?php echo ARCVERSION; ?>, Written by Craig Longford</p>
+    </div>
+</div>
+<?php system\Helper::arcGetFooter(); ?>
+</body>
+</html>
