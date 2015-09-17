1f61566390aaea57bc5ebbcb4eebeddf98cf10f7
clongford <clongford@CITGNOC2.concordeinf.com>
2015-09-17 14:15:26 +0100

    Page fix

--- app/modules/page/module.php
+++ app/modules/page/module.php
@@ -8,7 +8,7 @@ $defaultAction = SystemSetting::getByKey("ARC_DEFAULT_ACTION");
 
 $pages = Page::getAllPages();
 foreach ($pages as $page) {
-    if ($defaultModule->value == "page" && $defaultAction->value == $page->seourl || !isset($page->title))
+    if ($defaultModule->value == "page" && $defaultAction->value == $page->seourl || empty($page->title))
         continue;
     system\Helper::arcAddMenuItem($page->title, "fa-file-o", false, system\Helper::arcGetPath() . "page/{$page->seourl}", $setting->value);
 }
