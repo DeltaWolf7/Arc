15b96ef87595441cac6bf75ff6eab6e99549cc92
clongford <clongford@CITGPCNOC01.concordeinf.com>
2015-10-09 17:51:39 +0100

    Migration towards new system.

--- app/modules/user/js/user.js
+++ app/modules/user/js/user.js
@@ -14,8 +14,9 @@ $(document).ready(function () {
 
     // Register
     $("#registerBtn").click(function () {
-        arcAjaxRequest({firstname: $("#firstname").val(), lastname: $("#lastname").val(), email: $("#email").val(), password: $("#password").val(), password2: $("#password2").val()},
-        complete, null);
+        arcAjaxRequest({firstname: $("#firstname").val(), lastname: $("#lastname").val(),
+            email: $("#email").val(), password: $("#password").val(), password2: $("#password2").val()},
+                complete, null);
     });
 
     // Login
@@ -25,9 +26,10 @@ $(document).ready(function () {
 
     // Reset
     $("#btnReset").click(function () {
-        arcAjaxRequest({password: $("#password").val(), password2: $("#password2").val(), id: sid}, resetComplete, null)
+        arcAjaxRequest({password: $("#password").val(), password2: $("#password2").val(), id: sid},
+                resetComplete, null)
     });
-    
+
     // Forgot
     $("#forgotBtn").click(function () {
         arcAjaxRequest({email: $("#email").val()}, forgotComplete, null);
@@ -72,5 +74,156 @@ function forgotUpdateStatusCallback(data) {
     if (data.danger == 0) {
         $("#email").prop("disabled", true);
         $("#forgotBtn").prop("disabled", true);
+    }
+}
+
+
+
+// Administration
+var userid;
+var groupid;
+
+$(document).ready(function () {
+    get("users");
+
+    $("#addGroupBtn").click(function () {
+        if ($('#groups').val() != null) {
+            arcAjaxRequest({action: "addgroup", id: userid, group: $('#groups').val()}, addGrpComplete, null);
+            updateStatus("status", null);
+        }
+    });
+
+    $("#removeFromGroupBtn").click(function () {
+        if ($('#groups2').val() != null) {
+            arcAjaxRequest({action: "removefromgroup", id: userid, group: $('#groups2').val()}, remGrpComplete, null);
+            updateStatus("status", null);
+        }
+    });
+
+    $("#removeUserBtn").click(function () {
+        arcAjaxRequest({id: userid, action: "remove"}, remUserComplete, null);
+    });
+
+    $("#saveUserbtn").click(function () {
+        arcAjaxRequest({action: "saveuser", id: userid, firstname: $('#firstname').val(),
+            lastname: $('#lastname').val(), email: $('#email').val(),
+            password: $('#password').val(), retype: $('#retype').val(), enabled: $("#enabled").prop("checked")}, saveUserComplete, null);
+        updateStatus("status", updateStatusCallback);
+    });
+
+    $("#removeGroupDoBtn").click(function () {
+        arcAjaxRequest({id: groupid, action: "removegroup"}, removeGrpDoComplete, null);
+        updateStatus("status", null);
+    });
+
+    $("#saveGroupBtn").click(function () {
+        arcJaxRequest({action: "savegroup", id: groupid, name: $('#groupname').val(),
+            description: $('#groupdescription').val()}, saveGroupComplete, null);
+        updateStatus("status", updateStatusCallback2);
+    });
+});
+
+function get(action) {
+    if (action == "users") {
+        $("#tabUsers").attr("class", "active");
+        $("#tabGroups").removeClass("active");
+    } else {
+        $("#tabUsers").removeClass("active");
+        $("#tabGroups").attr("class", "active");
+    }
+    arcAjaxRequest({action: action}, null, getSuccess);
+}
+
+function getSuccess(data) {
+    var jdata = jQuery.parseJSON(JSON.stringify(data));
+    $('#data').html(jdata.html);
+}
+
+function addGrpComplete(data) {
+    editUser(userid);
+}
+
+function remGrpComplete(data) {
+    editUser(userid);
+}
+
+function editUser(id) {
+    userid = id;
+    arcAjaxRequest({action: "user", id: userid}, editUserComplete, editUserSuccess);
+}
+
+function editUserSuccess(data) {
+    var jdata = jQuery.parseJSON(JSON.stringify(data));
+    $('#firstname').val(jdata.firstname);
+    $('#lastname').val(jdata.lastname);
+    $('#email').val(jdata.email);
+    $('#password').val("");
+    $('#retype').val("");
+    $('#grp2').html(jdata.group);
+    $("#enabled").prop('checked', jdata.enabled);
+    if (jdata.email == "") {
+        $('#email').removeAttr("disabled");
+    } else {
+        $('#email').attr("disabled", "disabled");
+    }
+}
+
+function editUserComplete() {
+    $("#editUserModal").modal("show");
+}
+
+function removeUser(id) {
+    userid = id;
+    $("#removeUserModal").modal("show");
+}
+
+function remUserComplete() {
+    get("users");
+    $("#removeUserModal").modal("hide");
+}
+
+
+
+function saveUserComplete() {
+    get("users");
+}
+
+function updateStatusCallback(data) {
+    if (data.danger == 0) {
+        $("#editUserModal").modal("hide");
+    }
+}
+
+function editGroup(id) {
+    groupid = id;
+    arcJaxRequest({action: "group", id: groupid}, completeEditGroup, successEditGroup);
+}
+
+function completeEditGroup(data) {
+    var jdata = jQuery.parseJSON(JSON.stringify(data));
+    $('#groupname').val(jdata.name);
+    $('#groupdescription').val(jdata.description);
+}
+
+function successEditGroup(data) {
+    $("#editGroupModal").modal("show");
+}
+
+function removeGroup(id) {
+    groupid = id;
+    $("#removeGroupModal").modal("show");
+}
+function removeGrpDoComplete() {
+    get("groups");
+    $("#removeGroupModal").modal("hide");
+}
+
+function saveGroupComplete() {
+    get("groups");
+}
+
+function updateStatusCallback2(data) {
+    if (data.status == "success") {
+        $("#editGroupModal").modal("hide");
     }
 }
