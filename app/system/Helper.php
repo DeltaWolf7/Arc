67caeb6683d7e1679eecfff6f04fb0488df5e69a
delta <delta@X51>
2016-03-09 19:43:45 +0000

    Added header information to API.

--- app/system/Helper.php
+++ app/system/Helper.php
@@ -845,6 +845,10 @@ class Helper {
     }
 
     public static function arcAPICall() {
+        header("Access-Control-Allow-Orgin: *");
+        header("Access-Control-Allow-Methods: *");
+        header("Content-Type: application/json");
+
         $key = \SystemSetting::getByKey("ARC_APIKEY");
 
         if (isset($_POST["key"])) {
