3480b048b46e26c3d366b6d8541855dab350287b
User <User@Alienware>
2016-03-13 20:36:57 +0000

    Added missing sql script.

---
+++ sql/sm_items.sql
@@ -0,0 +1,9 @@
+--
+-- Table structure for table `sm_items`
+--
+
+CREATE TABLE IF NOT EXISTS `sm_items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `data` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
