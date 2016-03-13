bfe64a2bb00f81402d0e0e13638a41ede7ece1a9
delta <delta@X51>
2016-03-13 16:50:42 +0000

    Revert "Added header information to API."
    This reverts commit 67caeb6683d7e1679eecfff6f04fb0488df5e69a.

--- app/system/Helper.php
+++ app/system/Helper.php
@@ -845,10 +845,6 @@ class Helper {
     }
 
     public static function arcAPICall() {
-        header("Access-Control-Allow-Orgin: *");
-        header("Access-Control-Allow-Methods: *");
-        header("Content-Type: application/json");
-
         $key = \SystemSetting::getByKey("ARC_APIKEY");
 
         if (isset($_POST["key"])) {
