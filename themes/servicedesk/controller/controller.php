1e49f5726ef2442dc5132f18e97a92a3b4341050
delta <delta@X51>
2016-05-03 07:46:16 +0100

    Theme for new project.

---
+++ themes/servicedesk/controller/controller.php
@@ -0,0 +1,13 @@
+<?php
+    system\Helper::arcAddHeader("css", system\Helper::arcGetThemePath() . "css/simple-line-icons.css");
+    system\Helper::arcAddHeader("css", system\Helper::arcGetThemePath() . "css/animate.min.css");
+    system\Helper::arcAddHeader("css", system\Helper::arcGetThemePath() . "css/whirl.css");
+    system\Helper::arcAddHeader("css", system\Helper::arcGetThemePath() . "css/weather-icons.min.css");
+    system\Helper::arcAddHeader("css", system\Helper::arcGetThemePath() . "css/sweet-alert.css");
+    system\Helper::arcAddHeader("css", system\Helper::arcGetThemePath() . "css/app.css");
+    
+   
+    system\Helper::arcAddHeader("favicon", system\Helper::arcGetThemePath() . "images/logo-32x32.png");
+    
+    system\Helper::arcAddFooter("js", system\Helper::arcGetThemePath() . "js/app.js");
+    system\Helper::arcAddFooter("js", system\Helper::arcGetThemePath() . "js/sweet-alert.min.js");
