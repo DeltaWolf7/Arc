d6e40dfcce85f3eaae3ac83cec9decb489880474
delta <delta@X51>
2016-05-11 08:20:13 +0100

    Media manager start.

--- app/modules/mediamanager/view/manager.php
+++ app/modules/mediamanager/view/manager.php
@@ -5,19 +5,68 @@
     <div class="panel-body">
         <a class="btn btn-primary"><i class="fa fa-upload"></i> Upload</a>
         <a class="btn btn-primary"><i class="fa fa-folder"></i> New Folder</a>
-        <a class="btn btn-primary"><i class="fa fa-close"></i> Delete</a>
+        <a class="btn btn-primary"><i class="fa fa-recycle"></i> Delete</a>
     </div>
     <div class="panel-body">
-        <table class="table table-striped">
+        <i class="fa fa-home"></i> / assets
+    </div>
+    <div class="panel-body">
+        <table class="table table-striped" id="browser">
             <?php
-            $files = scandir(system\Helper::arcGetPath(true) . "assets");
-            foreach ($files as $file) {
-                echo "<tr><td>{$file}</td><td></td><td></td></tr>";
-            }
+            GetPath(system\Helper::arcGetPath(true) . "assets/");
             ?>
         </table>
     </div>
-    <div class="panel-footer">
-
-    </div>
 </div>
+
+<?php
+
+function GetPath($path) {
+    $files = scandir($path);
+    foreach ($files as $file) {
+        if ($file != "." && $file != "..") {
+            if (is_dir($path . $file)) {
+                $fi = new FilesystemIterator($path . $file, FilesystemIterator::SKIP_DOTS);
+                echo "<tr><td><i class=\"fa fa-folder\"></i> {$file}</td><td>" . iterator_count($fi) . " items</td><td>" . date("d M Y", filectime($path . $file)) . "</td></tr>";
+            } else {
+                echo "<tr><td><i class=\"fa fa-file\"></i> {$file}</td><td>" . FileSizeConvert(filesize($path . $file)) . "</td><td>" . date("d M Y", filectime($path . $file)) . "</td></tr>";
+            }
+        }
+    }
+}
+
+function FileSizeConvert($bytes) {
+    $bytes = floatval($bytes);
+    $arBytes = array(
+        0 => array(
+            "UNIT" => "TB",
+            "VALUE" => pow(1024, 4)
+        ),
+        1 => array(
+            "UNIT" => "GB",
+            "VALUE" => pow(1024, 3)
+        ),
+        2 => array(
+            "UNIT" => "MB",
+            "VALUE" => pow(1024, 2)
+        ),
+        3 => array(
+            "UNIT" => "KB",
+            "VALUE" => 1024
+        ),
+        4 => array(
+            "UNIT" => "B",
+            "VALUE" => 1
+        ),
+    );
+
+    foreach ($arBytes as $arItem) {
+        if ($bytes >= $arItem["VALUE"]) {
+            $result = $bytes / $arItem["VALUE"];
+            $result = str_replace(".", ",", strval(round($result, 2))) . " " . $arItem["UNIT"];
+            break;
+        }
+    }
+    return $result;
+}
+?>
