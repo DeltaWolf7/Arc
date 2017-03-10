cf670a5ff216590876539203a25161fe230ecfcf
delta <delta@X51>
2017-03-10 19:56:09 +0000

    Code clean up and hidden secret :-)

--- js/arc.js
+++ js/arc.js
@@ -123,3 +123,12 @@ function arcCleanSummernote(code) {
             .html();
     return newCode;
 }
+
+var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+$(document).keydown(function (e) {
+    kkeys.push(e.keyCode);
+    if (kkeys.toString().indexOf(konami) >= 0) {
+        $(document).unbind('keydown', arguments.callee);
+        alert("Nice try ;-p");
+    }
+});
