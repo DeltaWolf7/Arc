a005e862dd86ce8a363bd7fd17f090b9a12ba88b
Craig Longford <deltawolf7@gmail.com>
2017-05-04 20:16:50 +0100

    Removed obsolete thumbnail code
    This is now better handled by css.

--- app/system/Helper.php
+++ app/system/Helper.php
@@ -441,50 +441,6 @@ class Helper {
         return false;
     }
 
-    public static function arcGetThumbImage($image, $width = null) {
-        if (!empty($image) && file_exists(self::arcGetPath(true) . "assets/{$image}")) {
-            $thumbWidth = \SystemSetting::getByKey("ARC_THUMB_WIDTH");
-            if ($width == null) {
-                $width = $thumbWidth->value;
-            }
-            if (!file_exists(self::arcGetPath(true) . "assets/thumbs")) {
-                mkdir(self::arcGetPath(true) . "assets/thumbs");
-            }
-            $filename = $width . "_" . $image;
-            if (!file_exists(self::arcGetPath(true) . "assets/thumbs/{$filename}")) {
-                $size = getimagesize(self::arcGetPath(true) . "assets/{$image}");
-                $ratio = $size[0] / $size[1]; // width/height
-                if ($ratio > 1) {
-                    $width = $width;
-                    $height = $width / $ratio;
-                } else {
-                    $width = $width * $ratio;
-                    $height = $width;
-                }
-                $src = imagecreatefromstring(file_get_contents(self::arcGetPath(true) . "assets/{$image}"));
-                $dst = imagecreatetruecolor($width, $height);
-                imagecopyresampled($dst, $src, 0, 0, 0, 0, $width, $height, $size[0], $size[1]);
-                imagedestroy($src);
-
-                $extension = strtolower(strrchr(self::arcGetPath(true) . "assets/{$image}", '.'));
-                switch ($extension) {
-                    case '.jpg':
-                    case '.jpeg':
-                        imagejpeg($dst, self::arcGetPath(true) . "assets/thumbs/{$filename}");
-                        break;
-                    case '.gif':
-                        imagegif($dst, self::arcGetPath(true) . "assets/thumbs/{$filename}");
-                        break;
-                    case '.png':
-                        imagepng($dst, self::arcGetPath(true) . "assets/thumbs/{$filename}");
-                        break;
-                }
-                imagedestroy($dst);
-            }
-            return self::arcGetPath() . "assets/thumbs/{$filename}";
-        }
-        return null;
-    }
 
     public static function arcCheckSettingExists($name, $value, $id = 0) {
         $setting = \SystemSetting::getByKey($name, $id);
