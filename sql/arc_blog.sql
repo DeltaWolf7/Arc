af799250e8d0609ea322d4b0da9d84852536ee9e
Craig Longford <deltawolf7@gmail.com>
2017-05-28 16:08:20 +0100

    Added missing Sql changes
    Added missing sql changes for blog module

--- sql/arc_blog.sql
+++ sql/arc_blog.sql
@@ -2,18 +2,23 @@
 -- Table structure for table `arc_blog`
 --
 
-CREATE TABLE IF NOT EXISTS `arc_blog` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `arc_blog` (
+  `id` int(11) NOT NULL,
   `date` datetime NOT NULL,
   `title` varchar(100) NOT NULL,
   `content` text NOT NULL,
   `image` varchar(255) NOT NULL,
   `tags` text NOT NULL,
-  `poster` varchar(50) NOT NULL,
-  `category` varchar(100) NOT NULL,
-  `seourl` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `poster` int(11) NOT NULL,
+  `categoryid` int(11) NOT NULL,
+  `seourl` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `arc_blog`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `arc_blog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- Table structure for table `arc_blog_categories`
