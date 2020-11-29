c1e7785a2967603daf4718e8a73a7043c177248c
Craig Longford <deltawolf7@gmail.com>
2020-11-29 18:51:12 +0000

    Update Bootstrap to V4.5

--- app/system/Helper.php
+++ app/system/Helper.php
@@ -86,13 +86,13 @@ class Helper {
         // Javascript, add required javascript files to header
         self::arcAddFooter("external", "<script src=\"https://code.jquery.com/jquery-3.5.1.min.js\" integrity=\"sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=\" crossorigin=\"anonymous\"></script>");
         self::arcAddFooter("external", "<script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js\" integrity=\"sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo\" crossorigin=\"anonymous\"></script>");
-        self::arcAddFooter("external", "<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js\" integrity=\"sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI\" crossorigin=\"anonymous\"></script>");
+        self::arcAddFooter("external", "<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx\" crossorigin=\"anonymous\"></script>");
         self::arcAddFooter("external", "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js\"></script>");
         self::arcAddFooter("external", "<script src=\"https://kit.fontawesome.com/5ddfcf8ebf.js\" crossorigin=\"anonymous\"></script>");
         self::arcAddFooter("js", self::arcGetPath() . "vendor/arc/js/arc.js");
 
         // CSS, add required css files to header
-        self::arcAddHeader("external", "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css\" integrity=\"sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk\" crossorigin=\"anonymous\">");
+        self::arcAddHeader("external", "<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css\" integrity=\"sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2\" crossorigin=\"anonymous\">");
         self::arcAddHeader("css", self::arcGetPath() . "vendor/arc/css/arc.css");
     }
 
