045a7040b898bf37b47cf6610108ad278062efea
Craig Longford <deltawolf7@gmail.com>
2021-06-08 17:00:47 +0100

    updates to base
    and added domain checker tool.

---
+++ app/modules/adventure/view/game.php
@@ -0,0 +1,19 @@
+<div class="row">
+    <div class="col-md-8">
+        <div class="card">
+            <img src="<?php echo system\Helper::arcGetPath() . "assets/adventure/backgrounds/forest.png"; ?>"
+                class="card-img-top" alt="Adventure Scene">
+            <div class="card-body">
+                <h5 class="card-title">Rude Awakening</h5>
+                <p class="card-text"></p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Bag of holding</h5>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ themes/blankcanvas/js/main.js
@@ -0,0 +1,37 @@
+function arcNotification(data) {
+    switch (data.messages.type) {
+        case "danger":
+            title = "Error";
+            type = "error"
+            break;
+        case "primary":
+            title = "Information";
+            type = "info"
+            break;
+        case "warning":
+            title = "Warning";
+            type = "warning";
+            break;
+        case "wrong":
+            title = "Incorrect";
+            type = "error"
+            break;
+        case "right":
+            title = "Correct";
+            type = "success";
+            break;
+        default:
+            title = "Success";
+            type = "success";
+            break;
+    }
+
+	Swal.fire({
+		title: title,
+		text: data.messages.message,
+		icon: type,
+		confirmButtonText: 'OK'
+	  })
+}
+
+
