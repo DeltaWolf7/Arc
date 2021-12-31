2c9cd98e08ec7c7c2e89ca236a8cee7eb972030a
Craig Longford <deltawolf7@gmail.com>
2021-12-31 13:33:12 +0000

    fixed cron jobs

--- app/modules/arc/cron/clearlogs.php
+++ app/modules/arc/cron/clearlogs.php
@@ -5,6 +5,6 @@
     $days = SystemSetting::getByKey("ARC_KEEP_LOGS");
     // Delete logs older than the number of kept days    
     if ($days->value != "") {
-        system\Helper::arcGetDatabase()->query("delete from " . $log->table . " where datediff(now(), " . $log->table . ".event) > " . $days->value);
+        system\Helper::arcGetDatabase()->query("delete from arc_logs where datediff(now(), arc_logs.event) > " . $days->value);
         Log::createLog("success", "CRON", "Purged logs older than " . $days->value);
     }
