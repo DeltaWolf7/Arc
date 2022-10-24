7b4b46b786ab0e282b5f116a1439f76590b7b655
Craig Longford <deltawolf7@gmail.com>
2022-10-24 21:48:53 +0100

    latest

--- app/system/Helper.php
+++ app/system/Helper.php
@@ -51,7 +51,7 @@ class Helper {
         self::$arc["modulepath"] = "";
 
         // Version
-        self::$arc["version"] = "0.12.0.5";
+        self::$arc["version"] = "0.12.0.6";
 
         // Search Results Flag
         self::$arc["hassearchresults"] = false;
@@ -93,11 +93,11 @@ class Helper {
         }
 
         // Javascript, add required javascript files to header
-        self::arcAddFooter('external', '<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>');
-        self::arcAddFooter('external', '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>');
-        self::arcAddHeader('external', '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">');
-        self::arcAddFooter('external', '<script src="https://cdn.jsdelivr.net/npm/moment@2.29.1/moment.min.js" defer></script>');
-        self::arcAddFooter('external', '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />');
+        self::arcAddFooter('external', '<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>');
+        self::arcAddFooter('external', '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha256-h1OMS35Ij1pJ0S+Y1qBK/GHQDyankPMZVpeZrNQ062U=" crossorigin="anonymous"></script>');
+        self::arcAddHeader('external', '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha256-IUOUHAPazai08QFs7W4MbzTlwEWFo7z/4zw8YmxEiko=" crossorigin="anonymous">');
+        self::arcAddFooter('external', '<script src="https://cdn.jsdelivr.net/npm/moment@2.29.4/dist/moment.js" integrity="sha256-SFm2p+y/sQKIKPpUM8QTghWu5ks7TYqXUA0NeGIH8bc=" crossorigin="anonymous"></script>');
+        self::arcAddFooter('external', '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.0/css/all.min.css" integrity="sha256-AbA177XfpSnFEvgpYu1jMygiLabzPCJCRIBtR5jGc0k=" crossorigin="anonymous">');
         self::arcAddFooter('js', self::arcGetPath() . 'vendor/arc/js/arc.min.js');
 
         // CSS, add required css files to header
